df0f25f9c63bdbaa53327c31fe567841ced24271
bryanco03 <bryanco0310@gmail.com>
2024-01-23 12:20:21 -0600

    Documentacion
    Signed-off-by: bryanco03 <bryanco0310@gmail.com>

--- Tareas/TAREA_TRES/src/OperacionBasica.hpp
+++ Tareas/TAREA_TRES/src/OperacionBasica.hpp
@@ -26,18 +26,24 @@ class OperacionesBasicas{
         /**
          * @brief validarSumaResta() Verifica si las dimensiones de las dos matrices concuerdan para que sea valido la suma/resta
          * 
-         * @param Matriz<T>& matriz1 matriz a sumar/restar
+         * @param Matriz<T> matriz1 matriz a sumar/restar
          * 
-         * @param Matriz<T>& matriz2 matriz a sumar/restar
+         * @param Matriz<T> matriz2 matriz a sumar/restar
+         * 
+         * @return false Si la operacion no es valida
+         * @return true si la operacion es valida
          */
         bool validarSumaResta(const Matriz<T>& matriz1, const Matriz<T>& matriz2);
 
         /**
          * @brief validarMultiplicacion() Verifica si las dimensiones de las dos matrices concuerdan para que sea valido la multiplicacion
          * 
-         * @param Matriz<T>& matriz1 matriz a multiplicar
+         * @param Matriz<T> matriz1 matriz a multiplicar
          * 
-         * @param Matriz<T>& matriz2 matriz a multiplicar
+         * @param Matriz<T> matriz2 matriz a multiplicar
+         * 
+         * @return false Si la operacion no es valida
+         * @return true si la operacion es valida
          */
 
         bool validarMultiplicacion(const Matriz<T>& matriz1, const Matriz<T>& matriz2);
@@ -45,9 +51,12 @@ class OperacionesBasicas{
         /**
          * @brief suma() definicion de suma de matrices se suman elemento por elemento
          * 
-         * @param Matriz<T>& matriz1 matriz a sumar
+         * @param Matriz<T> matriz1 matriz a sumar
          * 
-         * @param Matriz<T>& matriz2 matriz a sumar
+         * @param Matriz<T> matriz2 matriz a sumar
+         * 
+         * @return Matriz<T> matriz resultado de la suma de matriz1 y matriz2
+         * 
          */
 
         Matriz<T> suma(const Matriz<T>& matriz1, const Matriz<T>& matriz2);
@@ -56,9 +65,12 @@ class OperacionesBasicas{
         /**
          * @brief resta() definicion de resta de matrices se restan elemento por elemento
          * 
-         * @param Matriz<T>& matriz1 matriz a restar
+         * @param Matriz<T> matriz1 matriz a restar
          * 
-         * @param Matriz<T>& matriz2 matriz a restar
+         * @param Matriz<T> matriz2 matriz a restar
+         * 
+         * @return Matriz<T> matriz resultado de la resta de matriz1 y matriz2
+         * 
          */
 
         Matriz<T> resta(const Matriz<T>& matriz1, const Matriz<T>& matriz2);
@@ -66,9 +78,12 @@ class OperacionesBasicas{
         /**
          * @brief multiplicacion() definicion de un producto de 2 matrices
          * 
-         * @param Matriz<T>& matriz1 matriz a multiplicar
+         * @param Matriz<T> matriz1 matriz a multiplicar
          * 
-         * @param Matriz<T>& matriz2 matriz a multiplicar
+         * @param Matriz<T> matriz2 matriz a multiplicar
+         * 
+         * @return Matriz<T> matriz resultado de el producto de matriz1 y matriz2
+         * 
          */
 
         Matriz<T> multiplicacion(const Matriz<T>& matriz1, const Matriz<T>& matriz2);
--- Tareas/TAREA_TRES/src/OperacionCompleja.hpp
+++ Tareas/TAREA_TRES/src/OperacionCompleja.hpp
@@ -1,20 +1,88 @@
+/**
+ * @file OperacionCompleja.hpp
+ * @author Bryan Cortés
+ * @brief Declaraciones de clase OperacionesComplejas
+ * @version 0.1
+ * @date 2024-01-23
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
 #ifndef OPERACIONESCOMPLEJAS_HPP
 #define OPERACIONESCOMPLEJAS_HPP
 #include <complex>
 #include"Matriz.hpp"
 
 
+/**
+ * @brief  Clase OperacionesComplejas, la cual implementa métodos para sumar, restar o multiplicar matrices con datos de tipo complex
+ * 
+ */
 class OperacionesComplejas{
 
         public:
-
+        /** 
+         * @brief validarSumaResta() Verifica si las dimensiones de las dos matrices concuerdan para que sea valido la suma/resta
+         * 
+         * @param Matriz<complex<double>> matriz1 matriz a sumar/restar
+         * 
+         * @param Matriz<complex<double>> matriz2 matriz a sumar/restar
+         * 
+         * @return false Si la operacion no es valida
+         * @return true si la operacion es valida
+         */
         bool validarSumaResta(const Matriz<complex<double>>& matriz1, const Matriz<complex<double>>& matriz2);
+
+        /** 
+         * @brief validarMultiplicacion() Verifica si las dimensiones de las dos matrices concuerdan para que sea valido la multiplicacion
+         * 
+         * @param Matriz<complex<double>> matriz1 matriz a multiplicar
+         * 
+         * @param Matriz<complex<double>> matriz2 matriz a sumar/restar
+         * 
+         * @return false Si la operacion no es valida
+         * @return true si la operacion es valida
+         */
+
 
         bool validarMultiplicacion(const Matriz<complex<double>>& matriz1, const Matriz<complex<double>>& matriz2);
 
+
+        /**
+         * @brief suma() definicion de suma de matrices se suman elemento por elemento
+         * 
+         * @param Matriz<complex<double>> matriz1 matriz a sumar
+         * 
+         * @param Matriz<complex<double>> matriz2 matriz a sumar
+         * 
+         * @return Matriz<complex<double>> matriz resultado de la suma de matriz1 y matriz2
+         */
+
         Matriz<complex<double>> suma(const Matriz<complex<double>>& matriz1, const Matriz<complex<double>>& matriz2);
 
+        /**
+         * @brief resta() definicion de suma de matrices se restan elemento por elemento
+         * 
+         * @param Matriz<complex<double>> matriz1 matriz a restar
+         * 
+         * @param Matriz<complex<double>> matriz2 matriz a restar
+         * 
+         * @return Matriz<complex<double>> matriz resultado de la resta de matriz1 y matriz2
+         */
+
         Matriz<complex<double>> resta(const Matriz<complex<double>>& matriz1, const Matriz<complex<double>>& matriz2);
+
+        /**
+         * @brief Multiplicacion() definicion de multiplicacion de dos matrices
+         * 
+         * @param Matriz<complex<double>> matriz1 matriz a multiplicar
+         * 
+         * @param Matriz<complex<double>> matriz2 matriz a multiplicar
+         * 
+         * @return Matriz<complex<double>> matriz resultado de el producto de matriz1 y matriz2
+         */
+
 
         Matriz<complex<double>> multiplicacion(const Matriz<complex<double>>& matriz1, const Matriz<complex<double>>& matriz2);
 
